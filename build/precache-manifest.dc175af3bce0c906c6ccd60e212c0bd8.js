7bc2ed1d1b10bb145ead8e56fa15071e7616c1ec
yilin xu <xu.yilin@husky.neu.edu>
2020-07-20 17:25:18 -0400

    add build

---
+++ build/precache-manifest.dc175af3bce0c906c6ccd60e212c0bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b185d72d9d6563a08d613494fca6a8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec16e2207bc37b60bffc",
+    "url": "/static/css/main.42bd943c.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec16e2207bc37b60bffc",
+    "url": "/static/js/main.a645b2db.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
