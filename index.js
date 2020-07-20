10c67eb5918fffedcacb5be00b7d1839d8405942
yilin xu <xu.yilin@husky.neu.edu>
2020-07-20 12:05:47 -0400

    ex3.3 update

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json());
 
 let persons = [
-  { name: "Arto Hellas", number: "040-123456" },
-  { name: "Ada Lovelace", number: "39-44-5323523" },
-  { name: "Dan Abramov", number: "12-43-234345" },
-  { name: "Mary Poppendieck", number: "39-23-6423122" },
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
 ];
 
 app.get("/", (req, res) => {
@@ -24,6 +24,16 @@ app.get("/info", (req, res) => {
       persons.length
     } people</p> <p>${new Date()}</p>`
   );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let person = persons.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001;
