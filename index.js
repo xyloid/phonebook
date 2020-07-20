d72b020017bdd9d87b74633b4c713208b9a842bd
yilin xu <xu.yilin@husky.neu.edu>
2020-07-20 12:08:47 -0400

    ex3.4 update

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  let id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
