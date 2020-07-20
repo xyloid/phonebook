997f332b749d5f5fc92472079e880541a21aafc9
yilin xu <xu.yilin@husky.neu.edu>
2020-07-20 11:54:49 -0400

    ex3.1 update

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  { name: "Arto Hellas", number: "040-123456" },
+  { name: "Ada Lovelace", number: "39-44-5323523" },
+  { name: "Dan Abramov", number: "12-43-234345" },
+  { name: "Mary Poppendieck", number: "39-23-6423122" },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Phonebook!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
