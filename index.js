8aa7c017971d0f0538003e9cfd818991703c5484
yilin xu <xu.yilin@husky.neu.edu>
2020-07-21 17:43:40 -0400

    ex3.15 update

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const morgan = require("morgan");
 app.use(express.json());
 
 const cors = require("cors");
+const { response } = require("express");
 
 app.use(cors());
 
@@ -25,12 +26,12 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
+// let persons = [
+//   { name: "Arto Hellas", number: "040-123456", id: 1 },
+//   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+//   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+//   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+// ];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello Phonebook!</h1>");
@@ -52,22 +53,26 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  let id = Number(req.params.id);
   // let person = persons.find((p) => p.id === id);
   // if (person) {
   //   res.json(person);
   // } else {
   //   res.status(404).json("person id doesn't exist");
   // }
-  Person.findById(id).then(person=>{
+  Person.findById(req.params.id).then(person=>{
     res.json(person);
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  let id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (request, response,next) => {
+  // let id = Number(req.params.id);
+  // persons = persons.filter((p) => p.id !== id);
+  // res.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+  .then(result=>{
+    response.status(204).end();
+  })
+  .catch(error=>next(error));
 });
 
 app.post("/api/persons", (req, res) => {
@@ -84,7 +89,6 @@ app.post("/api/persons", (req, res) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    id: generate_id(),
   });
 
   person.save().then((savePerson) => {
@@ -95,6 +99,23 @@ app.post("/api/persons", (req, res) => {
   // res.json(person);
 });
 
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
 const generate_id = () => {
   let id = 0;
   do {
@@ -102,6 +123,8 @@ const generate_id = () => {
   } while (persons.find((p) => p.id === id));
   return id;
 };
+
+
 
 // const PORT = process.env.PORT || 3001;
 const PORT = process.env.PORT;
