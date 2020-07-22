3e742aa8c97e7d70d3fa877aaead0fb1a69294a1
yilin xu <xu.yilin@husky.neu.edu>
2020-07-22 14:59:25 -0400

    update ex3.20 before app.update implemented validation

--- index.js
+++ index.js
@@ -121,10 +121,13 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error("print error msg", error.message);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    console.log("before send", error.message);
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 let uniqueValidator = require("mongoose-unique-validator");
 
@@ -15,8 +15,8 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true },
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 },
 });
 
 personSchema.plugin(uniqueValidator);
